3e9b42514dd22b2e7b28208c1eb798de389eed02
Klin-Coderse <obcbeats@gmail.com>
2022-10-28 15:18:07 +0300

    createUser

--- pages/api/createUser.js
+++ pages/api/createUser.js
@@ -1,13 +1,67 @@
-import { create } from "../../lib/dbApi";
+import { hashPassword } from "../../lib/authControllers";
+import { create, find } from "../../lib/dbApi";
+import { NextRequest } from "next/server";
+
+const requestIp = require("request-ip");
 
 const handler = async (req, res) => {
-  const response = await create("User", { name: "Alfred", role: "Admin" });
-  console.log(response);
+  const { method } = req;
 
-  res.status(201).json({
-    status: "success",
-    data: response,
-  });
+  const { geo, ip, ua } = NextRequest;
+
+  console.log(geo, ip, ua);
+  if (method !== "POST")
+    return res.status(400).json({
+      status: "Error",
+      message: "Invalid req method!, route expects POST req",
+    });
+
+  const { username, password } = req.body;
+
+  if (!username || !password)
+    return res.status(400).json({
+      status: "Error",
+      message: "Incomplete credentials!",
+    });
+
+  try {
+    // check if username has been taken
+    const existingUser = await find("User", {
+      username: username.toLowerCase(),
+    });
+
+    if (existingUser?.documents.length > 0)
+      return res.status(409).json({
+        status: "Error",
+        message: "Username has already been taken",
+      });
+
+    // // create a new user
+    const modifiedPass = await hashPassword(password);
+    const ipAddress = requestIp.getClientIp(req);
+    console.log(ipAddress);
+    const forwarded = req.headers["x-forwarded-for"];
+    const ip = forwarded
+      ? forwarded.split(/, /)[0]
+      : req.connection.remoteAddress;
+    console.log(ip);
+
+    const user = await create("User", {
+      username: username.toLowerCase(),
+      password: modifiedPass,
+      ...(ipAddress && { userIp: ipAddress }),
+    });
+
+    return res.status(201).json({
+      status: "Success",
+      user,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      status: "Error",
+      message: error.message,
+    });
+  }
 };
 
 export default handler;
