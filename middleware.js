89e64f8eb0acaeb28a97aa9bdc641ea40d9198f6
Klin-Coderse <obcbeats@gmail.com>
2022-10-28 15:51:10 +0300

    mid

--- middleware.js
+++ middleware.js
@@ -1,6 +1,8 @@
+import { NextResponse } from "next/server";
+
 export function middleware(req, res) {
   console.log(req);
   const { geo, ip, userAgent } = req;
-  console.log(geo, ip, ua);
-  return res.next();
+  console.log(geo, ip, userAgent);
+  return NextResponse.next();
 }
