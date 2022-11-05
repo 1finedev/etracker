2481db563a5e4154791f0ed7dfcd22013d8de500
Yusuf Bilal <ubahyusuf484@gmail.com>
2022-11-05 18:13:57 +0100

    added tailwindcss children plugin, extended colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#3A8600',
+        secondary: '#9F9F9F',
+        tertiary: '#E8E8E8'
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-children'),
+  ],
 };
