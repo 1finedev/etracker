496ed665c12a7d77dbd6142f15823bbf81547814
Yusuf Bilal <ubahyusuf484@gmail.com>
2022-11-07 10:52:55 +0100

    redesign alert component

--- components/Alert/index.jsx
+++ components/Alert/index.jsx
@@ -1,28 +1,28 @@
 import { useEffect } from "react";
-import { AlertDurationBarStyles, AlertStyles } from "./Alert.variant";
+import { AlertDurationBarStyles } from "./Alert.variant";
 
 const Alert = ({ intent, style, label, close, duration }) => {
-  useEffect(() => {
-    const timer = setTimeout(close, duration);
-    () => {
-      clearTimeout(timer);
-    };
-  }, [close, duration]);
+    useEffect(() => {
+        // const timer = setTimeout(close, duration);
+        () => {
+            clearTimeout(timer);
+        };
+    }, [close, duration]);
 
-  return (
-    <div className={AlertStyles({ intent })} style={style}>
-      <p className="font-bold flex-1 mx-2">{label}</p>
-      <button onClick={close} className="icon text-inherit">
-        <img src="/closeIcon.svg" alt="close" />
-      </button>
-      {!!duration && duration > 0 && (
-        <div
-          className={AlertDurationBarStyles({ intent })}
-          style={{ animationDuration: `${duration}ms` }}
-        />
-      )}
-    </div>
-  );
+    return (
+        <div className={"alert bg-white flex items-center transform -translate-x-1/2 max-w-[30rem] w-[90vw] h-16 rounded-md shadow-md overflow-hidden !z-[100000]"} style={style}>
+            {!!duration && duration > 0 && (
+                <div
+                    className={AlertDurationBarStyles({ intent })}
+                    style={{ animationDuration: `${duration}ms` }}
+                />
+            )}
+            <p className="font-bold flex-1 mx-2">{label}</p>
+            <button onClick={close} className="icon h-full px-4 text-xs text-slate-300 border border-l-slate-300">
+                CLOSE
+            </button>
+        </div>
+    );
 };
 
 export default Alert;
