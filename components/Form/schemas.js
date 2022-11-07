778d66eacc745ae2d1827e78ce8708d6e31e355c
Yusuf Bilal <ubahyusuf484@gmail.com>
2022-11-07 19:09:12 +0100

    created register schema

--- components/Form/schemas.js
+++ components/Form/schemas.js
@@ -4,3 +4,10 @@ export const loginSchema = Joi.object({
   username: Joi.string().alphanum().min(3).max(30).required(),
   password: Joi.string().min(6).required(),
 });
+
+export const registerSchema = Joi.object({
+  username: Joi.string().alphanum().min(3).max(30).required(),
+  displayName: Joi.string().alphanum().min(3).max(30).required(),
+  password: Joi.string().min(6).required(),
+  passwordConfirm: Joi.ref('password')
+});
